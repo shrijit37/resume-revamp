7fa955deaf6eb4fa4dc4d1181df5ebe023c1703a
Shrijit Srivastav <shrijit@Shrijits-MacBook-Pro.local>
2025-08-02 07:35:05 +0530

    work on dashboard started

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Hero } from './pages/Hero'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import { BrowserRouter, Routes, Route } from "react-router";
 import ResumeAnalysis from './pages/ResumeAnalysis';
 import Dashboard from './pages/Dashboard';
